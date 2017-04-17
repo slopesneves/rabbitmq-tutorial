e11d59bab46c532d0147901bd5db20ba4e837b38
slopesneves <lopes.neves.stephane@gmail.com>
2017-04-17 18:32:38 +0200

    Publish suscribe
    * Externalize messages util
    
    * Broadcast log message

--- src/main/java/com/rabbitmq/slopes/workqueues/NewTask.java
+++ src/main/java/com/rabbitmq/slopes/workqueues/NewTask.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
+import com.rabbitmq.slopes.StringUtils;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -19,27 +20,11 @@ public class NewTask {
             Channel channel = connection.createChannel();
 
             channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
-            String message = getMessage(args);
+            String message = StringUtils.getMessage(args);
             channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
 
             channel.close();
         }
-    }
-
-    private static String getMessage(String[] strings){
-        if (strings.length < 1)
-            return "Hello World!";
-        return joinStrings(strings, " ");
-    }
-
-    private static String joinStrings(String[] strings, String delimiter) {
-        int length = strings.length;
-        if (length == 0) return "";
-        StringBuilder words = new StringBuilder(strings[0]);
-        for (int i = 1; i < length; i++) {
-            words.append(delimiter).append(strings[i]);
-        }
-        return words.toString();
     }
 }
