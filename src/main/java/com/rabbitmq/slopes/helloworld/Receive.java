0494d61d052deeea3f8f2bf2621cae71cd91f1f4
Stephane LOPES-NEVES <stephane.lopes@metigate.com>
2017-04-17 11:48:11 +0200

    Hello world

---
+++ src/main/java/com/rabbitmq/slopes/helloworld/Receive.java
@@ -0,0 +1,30 @@
+package com.rabbitmq.slopes.helloworld;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import static com.rabbitmq.slopes.helloworld.Send.QUEUE_NAME;
+
+public class Receive {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
